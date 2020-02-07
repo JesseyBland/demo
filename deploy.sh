f01d33c5f85de3046397cf904710a7eac2ad4d2a
JesseyBlan <jesseybland@gmail.com>
2020-02-07 05:10:31 -0600

    more edits to server

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
-konsole -e docker run --rm -p 7777:7777 --name=server7777 demos2 &
-konsole -e docker run --rm -p 8888:7777 --name=server8888 demos2 &
-konsole -e docker run --rm -p 9999:7777 --name=server9999 demos2 &
+konsole -e docker run --rm -p 7777:7777 --name=server7777 demo3 &
+konsole -e docker run --rm -p 8888:7777 --name=server8888 demo3 &
+konsole -e docker run --rm -p 9999:7777 --name=server9999 demo3 &
